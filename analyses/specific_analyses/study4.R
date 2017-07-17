053bf6ab91627f055b2743fe3f7f79b87e84ad8a
Erin Bennett <erindb@stanford.edu>
2017-07-16 23:15:36 -0700

    fix ranking bug

--- analyses/specific_analyses/study4.R
+++ analyses/specific_analyses/study4.R
@@ -7,10 +7,6 @@ library(ordinal)
 library(languageR)
 library(mlogit)
 
-
-## load data
-# library(plyr)
-
 myCenter <- function(x) {
   if (is.numeric(x)) { return(x - mean(x)) }
   if (is.factor(x)) {
@@ -35,43 +31,34 @@ myCenter <- function(x) {
 
 # read in data
 d = read.csv("../data/study4_data.csv")
-total_ngrams = 1024908267229
-ngrams = read.csv("../data/web_1grams.csv")
-ngrams$surprisal = - (log(ngrams$frequency) - log(total_ngrams))
-
 
 
 n_nonenglish = length(unique((d %>%
                                 filter(!(language %in% c(
-                                  "English",  "english",  "Engli",    "Englsih",  "engish",   "English ",
+                                  "English",  "english",  "Engli", "en",
+                                  "Englsih",  "engish",   "English ", "Enlgish",
                                   "ENGLISH", "ENG", "eng"))))$workerid))
 n_did_not_follow_instructions = length(unique((d %>% filter(asses == "No"))$workerid))
 
 
+total_ngrams = 1024908267229
+ngrams = read.csv("../data/web_1grams.csv")
+ngrams$surprisal = - (log(ngrams$frequency) - log(total_ngrams))
+
 total_workers = length(unique(d$workerid))
-uncooperative = length(unique(subset(d, asses=="No")$workerid))
 d = subset(d, asses == "Yes" | asses == "Confused" | is.na(asses))
 good_workers = length(unique(d$workerid))
-print(total_workers)
-print(uncooperative)
-print(good_workers)
 
-
-df = d %>% filter(asses!="No" | is.na(asses))
-
-df = df[,c("workerid", "adverb", "ranking")]
-df$adverb = as.character(df$adverb)
-df$adjective = sapply(df$adverb, function(adv) {return(strsplit(adv, " ")[[1]][2])})
-df$adverb = sapply(df$adverb, function(adv) {return(strsplit(adv, " ")[[1]][1])})
-
-df$surprisal = sapply(df$adverb, function(adv) {return(ngrams$surprisal[adv == as.character(ngrams$ngram)][1])})
-df$syllables = sapply(df$adverb, function(adv) {return(ngrams$syllables[adv == as.character(ngrams$ngram)][1])})
-df$height_in_list = 9 - df$ranking
-df$adverb_list = rep("A", nrow(df))
-df$adverb_list[df$adverb %in% list_b] = "B"
-df$adverb_list[df$adverb %in% list_c] = "C"
-df$adverb_list[df$adverb %in% list_d] = "D"
-
+df = d %>%
+  mutate(adjective = char(adjective),
+         adverb = sapply(char(adverb), function(adv) {
+           return(strsplit(adv, " ")[[1]][1])
+         })) %>%
+  group_by(workerid, adjective) %>%
+  mutate(ranking = rank(ranking),
+         height_in_list = 9 - ranking) %>%
+  ungroup() %>% as.data.frame %>%
+  select(workerid, height_in_list, adverb, adjective)
 
 
 
@@ -125,6 +112,7 @@ aggdf %>%
   scale_colour_gradient(low="gray", high="black")
 ggsave("../edited_draft/images/plot_study4.pdf", width=5, height=2.5)
 
+
 novel = function(df) {
   df = df[(df$adverb %in% c("bugornly expensive", "tupabugornly expensive",
                             "ratumly expensive", "gaburatumly expensive",
@@ -157,35 +145,48 @@ novel = function(df) {
   df$length = sapply(as.character(df$adverb), function(adv) {return(adverb_length[adv])})
   df$root = sapply(as.character(df$adverb), function(adv) {return(adverb_root[adv])})
   
-  aggdf = df %>% group_by(adverb) %>%
-    summarise(
-      height_in_list_high = ci.high(height_in_list),
-      height_in_list_low = ci.low(height_in_list),
-      height_in_list = mean(height_in_list),
-      ranking_high = ci.high(ranking),
-      ranking_low = ci.low(ranking),
-      ranking = mean(ranking))
-  
+  aggdf = ddply(df, .(adverb), function(subd) {
+    resampled = boot(subd, function(orig, indices) {
+      return( c(
+        mean(orig[indices,]$height_in_list),
+        mean(orig[indices,]$ranking) )
+      )
+    }, 100)$t
+    newd = data.frame(
+      adverb = subd$adverb[[1]],
+      #     adjective = subd$adjective[[1]],
+      #     resid_surprisal = subd$resid_surprisal[[1]],
+      #     syllables = subd$syllables[[1]],
+      height_in_list = mean(subd$height_in_list),
+      height_in_list_high = quantile(resampled[,1], 0.025),
+      height_in_list_low = quantile(resampled[,1], 0.975),
+      ranking = mean(subd$ranking),
+      ranking_high = quantile(resampled[,2], 0.025),
+      ranking_low = quantile(resampled[,2], 0.975)
+    )
+    return(newd)
+  })
   # ggplot(aggdf, aes(x=adverb, y=height_in_list, colour=adverb)) +
   #   geom_point(size=3) +
   #   geom_errorbar(aes(x=adverb, ymin=height_in_list_low, ymax=height_in_list_high))
   
+  sink(file="output/Experiment4/novel-model.txt")
   df$ranking = ordered(df$ranking)
   df$height_in_list = ordered(df$height_in_list)
-  
-  
-  
   model = clm(height_in_list ~ length, data=df)
-  
   model_with_root = clm(height_in_list ~ length + root, data=df)
-  
   print(summary(model))
   print(summary(model_with_root))
-  
+  sink(NULL)
 }
 
 novel(d)
 
+df2 = df %>% group_by(adverb) %>%
+  summarise(ranking=mean(ranking))
+# df1b$intensifier[order(df1b$logprice)] %>%
+df2 %>% write.csv("intensifiers_mean_logprice_study2.csv",
+                  row.names=F)
 
 intensities = df %>% 
   rename(intensifier = adverb) %>%
@@ -196,40 +197,13 @@ intensities = df %>%
     mean_height_in_list = mean(height_in_list)) %>%
   mutate(
     low = (low - 
-      mean(mean_height_in_list))/sd(mean_height_in_list),
+             mean(mean_height_in_list))/sd(mean_height_in_list),
     high = (high - 
-      mean(mean_height_in_list))/sd(mean_height_in_list),
+              mean(mean_height_in_list))/sd(mean_height_in_list),
     intensity = (mean_height_in_list -
-      mean(mean_height_in_list))/sd(mean_height_in_list)) %>%
+                   mean(mean_height_in_list))/sd(mean_height_in_list)) %>%
   select(intensifier, intensity, low, high)
 intensities = intensities[order(intensities$intensity),]
 write.csv(intensities, "intensities_study4.csv", row.names=F)
 
-
-
-
-df = filler_df
-
-df$ch = df$height_in_list + 1
-df$chid = paste(df$adverb_list, df$workerid)
-
-
-G <- mlogit.data(df, choice = "ch", shape = "long", chid.var="chid",
-                 alt.var="adverb", ranked = TRUE)
-
-
-m_colinear = mlogit(ch ~ surprisal + syllables | 0, G)
-model_with_adj_interaction = mlogit(ch ~ surprisal + syllables + surprisal:adjective + syllables:adjective | 0, G)
-
-message("running simplified models...")
-m_only_syll = mlogit(ch ~ syllables | 0, G)
-m_only_surp = mlogit(ch ~ surprisal | 0, G)
-
-message("running residualized models...")
-m_resid_syll = mlogit(ch ~ surprisal + syll_resid | 0, G)
-m_resid_surp = mlogit(ch ~ surprisal_resid + syllables | 0, G)
-
-message("running likelihood ratio tests...")
-# anova(m_colinear)
-lr_diff_due_to_syll = lrtest(m_colinear, m_only_surp)
-lr_diff_due_to_surp = lrtest(m_colinear, m_only_syll)
+detach(package:plyr, unload=T)
